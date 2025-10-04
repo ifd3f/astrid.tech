1c5dbf55c6d53b740d249bd3e598e11c3478d30f
Astrid Yu <astrid@astrid.tech>
2025-10-04 15:08:11 -0700

    simplify flake

--- flake.nix
+++ flake.nix
@@ -1,74 +1,22 @@
 {
-  description = "QR Tattoo Redirector";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     naersk.url = "github:nix-community/naersk/master";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, naersk, ... }:
-    let inherit (nixpkgs) lib;
-    in {
-      overlays = {
-        default = final: prev: {
-          inherit (self.packages.${prev.system}) armqr;
-        };
-
-        build = lib.composeManyExtensions [
-          rust-overlay.overlays.default
-          naersk.overlay
-          (final: prev: {
-            rust-toolchain = final.rust-bin.nightly."2023-08-16";
-            naersk = prev.naersk.override {
-              rustc = final.rust-toolchain.minimal;
-              cargo = final.rust-toolchain.minimal;
-            };
-
-            armqr = with final;
-              final.naersk.buildPackage {
-                src = ./.;
-
-                buildInputs = [ openssl.dev ];
-                nativeBuildInputs = [ pkgconfig ];
-                PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-              };
-          })
-        ];
-      };
-    } // utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.build ];
-        };
-      in rec {
-        packages = {
-          default = packages.armqr;
-          armqr = pkgs.armqr;
-          vm-demo = self.nixosConfigurations.demo.config.system.build.vm;
-        };
-
-        apps = {
-          armqr = utils.lib.mkApp {
-            name = "armqr";
-            drv = packages.armqr;
-          };
-          default = apps.armqr;
-        };
-
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in {
+        packages.default = naersk-lib.buildPackage ./.;
         devShells.default = with pkgs;
           mkShell {
-            buildInputs = [
-              (rust-toolchain.default.override {
-                extensions = [ "rust-src" "rust-analysis" "clippy" "rustfmt" ];
-              })
-            ];
+            buildInputs =
+              [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
           };
       });
 }
